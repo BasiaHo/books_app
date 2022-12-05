d04147ffcb7f7d51b0415a69b7cfc0a809ea190e
Basia Hornuk <barbara.hornuk@outlook.com>
2022-12-05 15:20:25 +0100

    addToFavorite function

--- src/js/script.js
+++ src/js/script.js
@@ -13,6 +13,7 @@
     },
     books: {
       list: '.books-list',
+      bookImage: 'a.book__image',
     },
   };
 
@@ -23,7 +24,6 @@
   };
 
   function renderBooksList() {
-
     for (let book of dataSource.books) {
       const generatedHTML = templates.bookTemplate(book);
       const domElement = utils.createDOMFromHTML(generatedHTML);
@@ -33,5 +33,31 @@
   }
 
   renderBooksList();
-  
+
+  let favoritesBooks = [];
+
+  function addToFavorites(event) {
+    const clickedBook = this;
+    event.preventDefault();
+
+    const bookId = clickedBook.getAttribute('data-id');
+    const bookFavorite = favoritesBooks.indexOf(bookId);
+
+    if (bookFavorite < 0) {
+      favoritesBooks.push(bookId);
+    } else if (bookFavorite >= 0) {
+      favoritesBooks.splice(bookFavorite, 1);
+    }
+    console.log(favoritesBooks);
+    clickedBook.classList.toggle('favorite');
+  }
+
+  function initAction() {
+    const bookImage = document.querySelectorAll(select.books.bookImage);
+    for (image of bookImage) {
+      image.addEventListener('dblclick', addToFavorites);
+    }
+  }
+
+  initAction();
 }
